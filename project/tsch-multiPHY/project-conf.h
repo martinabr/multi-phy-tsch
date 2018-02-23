7cce1b36df6a0ec299b23aba7d11cbfae19c79e4
Simon Duquennoy <simon.duquennoy@ri.se>
2018-02-23 08:48:30 -0800

    Added multiPHY example

---
+++ project/tsch-multiPHY/project-conf.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2016, Inria.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         Project config file
+ * \author
+ *         Simon Duquennoy <simon.duquennoy@inria.fr>
+ *
+ */
+
+#ifndef __PROJECT_CONF_H__
+#define __PROJECT_CONF_H__
+
+/* Logging */
+#define LOG_CONF_LEVEL_MAC LOG_LEVEL_WARN
+#define TSCH_LOG_CONF_PER_SLOT 1
+#define LOG_CONF_WITH_COMPACT_ADDR 1
+
+/* IEEE802.15.4 PANID */
+#define IEEE802154_CONF_PANID 0xdf10
+
+/* Use multiradio: both cc1200 and cc2538 */
+#define NETSTACK_CONF_RADIO cc1200_driver
+#define TSCH_CONF_SCANNING_RADIO cc1200_driver /* used with CC1200_CONF_RF_CFG */
+//#define CC1200_CONF_RF_CFG cc1200_868_2gfsk_1_2kbps_sp /* sp: short packets */
+#define CC1200_CONF_RF_CFG cc1200_868_2gfsk_50kbps_802154g
+//#define CC1200_CONF_RF_CFG cc1200_868_2gfsk_250kbps
+//#define CC1200_CONF_RF_CFG cc1200_868_4gfsk_1000kbps
+#define ANTENNA_SW_SELECT_DEF_CONF  ANTENNA_SW_SELECT_SUBGHZ
+
+/* Make room in timeslot template for radio reconfig */
+#define TSCH_WITH_CC1200_RECONF 1
+/* Tell TSCH to use multiple radios */
+//#define TSCH_WITH_MULTIRADIO 1
+/* Do not start TSCH at init, wait for NETSTACK_MAC.on() */
+#define TSCH_CONF_AUTOSTART 0
+/* No 6TiSCH minimal schedule */
+#define TSCH_SCHEDULE_CONF_WITH_6TISCH_MINIMAL 0
+/* We will need more than 32 links */
+#define TSCH_SCHEDULE_CONF_MAX_LINKS 60
+#define TSCH_CONF_SYNC_WITH_LOWER_NODE_ID 1
+#define EB_ONLY_COORDINATOR 0
+
+/* EB and KA */
+#define TSCH_CONF_EB_PERIOD (4 * CLOCK_SECOND)
+#define TSCH_CONF_MAX_EB_PERIOD (4 * CLOCK_SECOND)
+#define TSCH_CONF_KEEPALIVE_TIMEOUT 0
+#define TSCH_CONF_MAX_KEEPALIVE_TIMEOUT 0
+#define TSCH_CONF_DESYNC_THRESHOLD (90 * CLOCK_SECOND)
+
+#define TSCH_CONF_DEFAULT_HOPPING_SEQUENCE (uint8_t[]){ 0, 1 }
+
+#endif /* __PROJECT_CONF_H__ */
---
+++ project/tsch-multiPHY/tsch-multiPHY.c
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2015, Zolertia - http://www.zolertia.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+/*---------------------------------------------------------------------------*/
+/**
+ * \addtogroup zoul-cc1200-demo Zoul on-board CC1200 RF transceiver test
+ *
+ * Demonstrates the use of the TI CC1200 RF transceiver on Sub-1GHz
+ * @{
+ *
+ * \file
+ *         Test file for the CC1200 demo
+ *
+ * \author
+ *         Antonio Lignan <alinan@zolertia.com>
+ */
+/*---------------------------------------------------------------------------*/
+#include "contiki.h"
+#include "sys/etimer.h"
+#include "net/nullnet/nullnet.h"
+#include "net/netstack.h"
+#include "net/packetbuf.h"
+#include "deployment/deployment.h"
+#include "net/mac/tsch/tsch-schedule.h"
+
+#include <stdio.h>
+#include <stdint.h>
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
+
+/* Same interval for data packets as we have for EBs */
+#define LOOP_INTERVAL       (TSCH_CONF_EB_PERIOD)
+#define TSCH_COORDINATOR_ID 1
+
+#include "cc1200-rf-cfg.h"
+extern const cc1200_rf_cfg_t cc1200_868_4gfsk_1000kbps;
+extern const cc1200_rf_cfg_t cc1200_868_2gfsk_50kbps_802154g;
+
+/*---------------------------------------------------------------------------*/
+//static struct etimer et;
+/*---------------------------------------------------------------------------*/
+void
+input_callback(const void *data, uint16_t len,
+  const linkaddr_t *src, const linkaddr_t *dest)
+{
+  LOG_INFO("Received seq %u from ", (unsigned)(*(uint32_t *)data));
+  LOG_INFO_LLADDR(src);
+  LOG_INFO_(" rssi %d\n",
+    (int8_t)packetbuf_attr(PACKETBUF_ATTR_RSSI)
+  );
+}
+/*---------------------------------------------------------------------------*/
+PROCESS(cc1200_demo_process, "cc1200 demo process");
+AUTOSTART_PROCESSES(&cc1200_demo_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(cc1200_demo_process, ev, data)
+{
+  static uint32_t count = 0;
+  struct tsch_slotframe *sf_cc1200_50;
+  struct tsch_slotframe *sf_cc1200_1000;
+  int i;
+
+  PROCESS_BEGIN();
+
+  /* Initialize NullNet */
+  nullnet_buf = (uint8_t *)&count;
+  nullnet_len = sizeof(count) + 8; /* Add 8 bytes to match the EB len*/
+  nullnet_set_input_callback(input_callback);
+
+
+  sf_cc1200_50 = tsch_schedule_add_slotframe(0, 53);
+  sf_cc1200_50->cc1200_config = &cc1200_868_2gfsk_50kbps_802154g;
+
+  sf_cc1200_1000 = tsch_schedule_add_slotframe(1, 53);
+  sf_cc1200_1000->cc1200_config = &cc1200_868_4gfsk_1000kbps;
+
+  tsch_schedule_add_link(sf_cc1200_50,
+      LINK_OPTION_TX,
+      LINK_TYPE_ADVERTISING, &tsch_broadcast_address,
+      node_id - 1, 0);
+  // tsch_schedule_add_link(sf_cc1200_50,
+  //     LINK_OPTION_SAMPLE_RSSI,
+  //     LINK_TYPE_ADVERTISING, &tsch_broadcast_address,
+  //     25, 0);
+  for(i = 0; i < 25; i++) {
+    if(i != node_id - 1) {
+      tsch_schedule_add_link(sf_cc1200_50,
+          LINK_OPTION_TIME_KEEPING | LINK_OPTION_RX,
+          LINK_TYPE_ADVERTISING, &tsch_broadcast_address,
+          i, 0);
+    }
+  }
+  tsch_schedule_add_link(sf_cc1200_1000,
+      LINK_OPTION_TX,
+      LINK_TYPE_ADVERTISING, &tsch_broadcast_address,
+      26 + node_id - 1, 0);
+  // tsch_schedule_add_link(sf_cc1200_1000,
+  //     LINK_OPTION_SAMPLE_RSSI,
+  //     LINK_TYPE_ADVERTISING, &tsch_broadcast_address,
+  //     26 + 25, 0);
+  for(i = 0; i < 25; i++) {
+    if(i != node_id - 1) {
+      tsch_schedule_add_link(sf_cc1200_1000,
+          LINK_OPTION_RX,
+          LINK_TYPE_ADVERTISING, &tsch_broadcast_address,
+          26 + i, 0);
+    }
+  }
+
+  /* Initialize TSCH */
+  tsch_set_coordinator(node_id == TSCH_COORDINATOR_ID);
+  NETSTACK_MAC.on();
+
+  // etimer_set(&et, LOOP_INTERVAL);
+  // while(1) {
+  //   PROCESS_YIELD();
+  //   if(ev == PROCESS_EVENT_TIMER) {
+  //     LOG_INFO("Sending seq %u\n", (unsigned)count);
+  //     NETSTACK_NETWORK.output(NULL);
+  //     count++;
+  //     etimer_reset(&et);
+  //   }
+  // }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+/**
+ * @}
+ */
