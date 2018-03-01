c8633145593977e4855c5a079c73f7549559056b
Simon Duquennoy <simon.duquennoy@ri.se>
2018-03-01 07:04:21 -0800

    Hopping sequence

--- project/tsch-multiPHY/project-conf.h
+++ project/tsch-multiPHY/project-conf.h
@@ -80,6 +80,10 @@
 #define TSCH_CONF_DESYNC_THRESHOLD (90 * CLOCK_SECOND)
 
 //#define TSCH_CONF_DEFAULT_HOPPING_SEQUENCE (uint8_t[]){ 0, 1 }
-#define TSCH_CONF_DEFAULT_HOPPING_SEQUENCE (uint8_t[]){ 0 }
+#define TSCH_CONF_DEFAULT_HOPPING_SEQUENCE (uint8_t[]){ \
+   0,  1,  2,  3,  4,  5,  6,  7,  8,  9, \
+  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, \
+  20, 21, 22, 23, 24, 25, 26, 27, 28, 29, \
+  30, 31, 32, 33 } \
 
 #endif /* __PROJECT_CONF_H__ */
